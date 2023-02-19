39bda324c4b9e15bb1f4359215ebccada3c0bb93
Viral Damaniya <damaniyaviral@gmail.com>
2023-02-18 19:50:35 -0800

    login

--- App.tsx
+++ App.tsx
@@ -1,8 +1,25 @@
-import React from "react";
-import MainScreen from "./screens/MainScreen";
+import React, { Component, useState, useEffect } from 'react';
+import { onAuthStateChanged } from "firebase/auth";
+import { MainScreen } from "./screens";
+import { auth } from './firebase';
+import SignUp from './components/SignUp';
 
-const App = () => (
-  <MainScreen/>
-);
+const App = () => {
+  const [isLogedIn, setIsLogedIn] = useState(false);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        console.log(user);
+        setIsLogedIn(true);
+      } else {
+        console.log("not loged in");
+      }
+  }); 
+  },[]);
+  return isLogedIn ? <MainScreen/> : <SignUp/>
+};
 
 export default App;
